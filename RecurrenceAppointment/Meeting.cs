00dfb4a613cfb40f6712a7ea8556798ac93a2eb3
Mohanraj-Guna <45057763+Mohanraj-Guna@users.noreply.github.com>
2019-10-09 10:35:12 +0530

    Add files via upload

---
+++ RecurrenceAppointment/Meeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RecurrenceAppointment
+{
+    /// <summary>
+    /// Represents custom data properties.
+    /// </summary>
+    public class Meeting
+    {
+        public string EventName { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public Brush Color { get; set; }
+        public string RecurrenceRule { get; set; }
+        public bool IsRecursive { get; set; }
+    }
+}
